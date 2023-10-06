303fa5116eebaf041a270339495941d4f67de9c7
Egor Ternovoy <cofob@riseup.net>
2023-10-06 18:59:11 +0700

    Add CI and error handler

--- src/creeps/creeps.ts
+++ src/creeps/creeps.ts
@@ -5,52 +5,52 @@ import { BuilderRole, HarvesterRole, RepairerRole, UpgraderRole } from "./roles"
 const logger = new Logger("creeps.creeps");
 
 function formatCreepName(name: string) {
-  return `${name}-${Game.time}`;
+    return `${name}-${Game.time}`;
 }
 
 export const HarvesterCreep: CreepSpawn = {
-  body: [WORK, CARRY, MOVE, MOVE],
-  name: "Harvester",
-  opts: { memory: { role: CreepRole.harvester } },
-  getCreepName: () => formatCreepName("Harvester"),
-  worker: new HarvesterRole(),
+    body: [WORK, CARRY, MOVE, MOVE],
+    name: "Harvester",
+    opts: { memory: { role: CreepRole.harvester } },
+    getCreepName: () => formatCreepName("Harvester"),
+    worker: new HarvesterRole(),
 };
 
 export const UpgraderCreep: CreepSpawn = {
-  body: [WORK, CARRY, CARRY, MOVE, MOVE],
-  name: "Upgrader",
-  opts: { memory: { role: CreepRole.upgrader } },
-  getCreepName: () => formatCreepName("Upgrader"),
-  worker: new UpgraderRole(),
+    body: [WORK, CARRY, CARRY, MOVE, MOVE],
+    name: "Upgrader",
+    opts: { memory: { role: CreepRole.upgrader } },
+    getCreepName: () => formatCreepName("Upgrader"),
+    worker: new UpgraderRole(),
 };
 
 export const BuilderCreep: CreepSpawn = {
-  body: [WORK, CARRY, MOVE, MOVE],
-  name: "Builder",
-  opts: { memory: { role: CreepRole.builder } },
-  getCreepName: () => formatCreepName("Builder"),
-  worker: new BuilderRole(),
+    body: [WORK, CARRY, MOVE, MOVE],
+    name: "Builder",
+    opts: { memory: { role: CreepRole.builder } },
+    getCreepName: () => formatCreepName("Builder"),
+    worker: new BuilderRole(),
 };
 
 export const RepairerCreep: CreepSpawn = {
-  body: [WORK, WORK, CARRY, MOVE, MOVE],
-  name: "Repairer",
-  opts: { memory: { role: CreepRole.repairer } },
-  getCreepName: () => formatCreepName("Repairer"),
-  worker: new RepairerRole(),
+    body: [WORK, WORK, CARRY, MOVE, MOVE],
+    name: "Repairer",
+    opts: { memory: { role: CreepRole.repairer } },
+    getCreepName: () => formatCreepName("Repairer"),
+    worker: new RepairerRole(),
 };
 
 export const roleToCreep = {
-  harvester: HarvesterCreep,
-  upgrader: UpgraderCreep,
-  builder: BuilderCreep,
-  repairer: RepairerCreep,
+    harvester: HarvesterCreep,
+    upgrader: UpgraderCreep,
+    builder: BuilderCreep,
+    repairer: RepairerCreep,
 };
 
 export function spawnCreep(world: string, creep_spawn: CreepSpawn): void {
-  let name = creep_spawn.getCreepName();
-  let result = Game.spawns[world].spawnCreep(creep_spawn.body, name, creep_spawn.opts);
-  if (result === OK) {
-    logger.info(`Spawning new creep: ${name}`);
-  }
+    let name = creep_spawn.getCreepName();
+    let result = Game.spawns[world].spawnCreep(creep_spawn.body, name, creep_spawn.opts);
+    if (result === OK) {
+        logger.info(`Spawning new creep: ${name}`);
+    }
 }
